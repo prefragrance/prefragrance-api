ca6cf8210ef96443dd49989faecd0b496f8b107c
Jin409 <sualng123@gmail.com>
2022-08-13 17:00:51 +0900

    initial commit

---
+++ config/urls.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="prefragrance",
+        default_version="v1",
+        description="API description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="prefragrance@gmail.com"),
+        license=openapi.License(name="MIT LICENSE"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        r"swagger(?P<format>\.json|\.yaml)",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        r"swagger",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        r"redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc-v1"
+    ),
+]
