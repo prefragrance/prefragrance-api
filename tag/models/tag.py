b08bad27bbee6083789fef300d120c7046cbd618
Jin409 <sualng123@gmail.com>
2022-08-19 00:16:54 +0900

    fix: tag cnt 필드 추가

--- tag/models/tag.py
+++ tag/models/tag.py
@@ -5,3 +5,7 @@ from django.db import models
 class Tag(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False) # primary key
     name = models.CharField(max_length = 200, null = False, blank = False)
+    cnt = models.IntegerField(
+        default=0,
+        blank=False,
+    )
