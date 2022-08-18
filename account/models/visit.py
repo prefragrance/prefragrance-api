dca2f9893fda1ed470ddcd5692d2245542406396
Jin409 <sualng123@gmail.com>
2022-08-19 00:08:02 +0900

    feat: account/visit 모델 추가

---
+++ account/models/visit.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Visit(models.Model):
+    """Model definition for Visit"""
+
+    user = models.ForeignKey(
+        'account.User',
+        on_delete=models.CASCADE,
+    )
+
+    product = models.ForeignKey(
+        'product.Product',
+        on_delete=models.CASCADE,
+    )
+
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+
+    class Meta:
+        """Meta definition for Visit."""
+
+        verbose_name = "Visit"
+        verbose_name_plural = "Visits"
+        db_table = "visits"
+
