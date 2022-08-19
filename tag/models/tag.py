d2aabf44a13c91a87743c09fd0e9b78ba737178f
Jin409 <sualng123@gmail.com>
2022-08-19 16:37:34 +0900

    fix: tag/cnt 필드 삭제

--- tag/models/tag.py
+++ tag/models/tag.py
@@ -5,7 +5,3 @@ from django.db import models
 class Tag(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False) # primary key
     name = models.CharField(max_length = 200, null = False, blank = False)
-    cnt = models.IntegerField(
-        default=0,
-        blank=False,
-    )
