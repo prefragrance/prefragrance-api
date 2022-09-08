2955bdde579f2acd99f19abccf1b191f0f9e5b96
Jin409 <sualng123@gmail.com>
2022-09-08 18:06:27 +0900

    feat: 인기검색어 구현

--- search/models/__init__.py
+++ search/models/__init__.py
@@ -1,2 +1,3 @@
 from .search import Search
 from .recommend_search import RecommendSearch
+from .search_log import SearchLog
---
+++ search/models/search_log.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+# 검색의 로그를 위한 모델
+
+class SearchLog(models.Model):
+    """Model definition for SearchLog"""
+
+    content = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        """Meta definition for SearchLog."""
+
+        verbose_name = "SearchLog"
+        verbose_name_plural = "SearchLogs"
+        db_table = "search_logs"
