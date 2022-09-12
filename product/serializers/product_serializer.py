39bd5093cb8e0b5dd7b8d28a81deb2eee0a7a92a
leonroars <comfynumb@hufs.ac.kr>
2022-09-12 23:49:32 +0900

    feat: product_serializer category 이름도 넘어가도록 수정

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -1,23 +1,58 @@
 from rest_framework.serializers import ModelSerializer
 
-from product.models import Product
-
+from product.models import Category, Product
 from tag.serializers import TagSerializer
+
+
+class CategorySerializer(ModelSerializer):
+    """Serializer definition for Category Model"""
+
+    model = Category
+    fields = ["name"]
+    read_only_fields = ["name"]
+
 
 class ProductSerializer(ModelSerializer):
     """Serializer definition for Product Model."""
 
     tags = TagSerializer(
-        read_only = False,
-        many = True,
+        read_only=False,
+        many=True,
+    )
+
+    category = CategorySerializer(
+        read_only=True,
+        many=False,
     )
 
     class Meta:
         """Meta definition for ProductSerializer."""
 
         model = Product
-        fields = ["id", "name", "producer", "feedback_cnt", "review_cnt",
-                  "visit_cnt", "thumbnail_url", "rate_sum", "rate", "tags"]
+        fields = [
+            "id",
+            "name",
+            "producer",
+            "category",
+            "feedback_cnt",
+            "review_cnt",
+            "visit_cnt",
+            "thumbnail_url",
+            "rate_sum",
+            "rate",
+            "tags",
+        ]
 
-        read_only_fields = ["id", "name", "producer", "feedback_cnt", "review_cnt",
-                  "visit_cnt", "thumbnail_url", "rate_sum", "rate",  "tags"]
+        read_only_fields = [
+            "id",
+            "name",
+            "producer",
+            "category",
+            "feedback_cnt",
+            "review_cnt",
+            "visit_cnt",
+            "thumbnail_url",
+            "rate_sum",
+            "rate",
+            "tags",
+        ]
