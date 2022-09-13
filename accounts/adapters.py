2b7792489fb189696b5c17f8609a32fc77ad1cc0
yoojung.choi <green980611@naver.com>
2022-09-13 23:57:29 +0900

    docs: 관련 없는 주석 삭제

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -4,7 +4,6 @@ from allauth.account.adapter import DefaultAccountAdapter
 class CustomAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=True):
         data = form.cleaned_data
-        # 기본 저장 필드: first_name, last_name, username, email
         user = super().save_user(request, user, form, False)
         user.nickname = data.get("nickname")
         user.name = data.get("name")
