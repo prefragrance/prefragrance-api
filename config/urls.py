a31712756e74fec7d403c8d8a65a5d514af247a3
Jin409 <sualng123@gmail.com>
2022-09-23 23:12:49 +0900

    fix: swagger 메인으로

--- config/urls.py
+++ config/urls.py
@@ -23,12 +23,12 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        r"swagger(?P<format>\.json|\.yaml)",
+        r"?P<format>\.json|\.yaml",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
     path(
-        r"swagger",
+        '',
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
