87179649af3b894858bf37b13e28f8dace91b236
Jin409 <sualng123@gmail.com>
2022-09-25 14:30:38 +0900

    chore: 피드백 반영

--- product/views/product_hot_api_view.py
+++ product/views/product_hot_api_view.py
@@ -1,17 +1,17 @@
+from collections import Counter
+from datetime import datetime, timedelta
 from itertools import product
-from rest_framework.generics import ListAPIView
+
 from rest_framework import status
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from datetime import datetime, timedelta
-from collections import Counter
-from drf_yasg.utils import swagger_auto_schema
-
+from accounts.models import Visit
 from product.models import Product
 from product.serializers import ProductSerializer
-from accounts.models import Visit
 from review.models import Review
+
 
 class ProductHotAPIView(ListAPIView):
     permission_classes = [AllowAny]
@@ -31,24 +31,22 @@ class ProductHotAPIView(ListAPIView):
         end_date = datetime.today()
         start_date = end_date - timedelta(days=7)
 
-        if standard == 'visit':
+        if standard == "visit":
             product_ids = Visit.objects.filter(
                 pub_date__range=[start_date, end_date]
-            ).values_list('product__id',flat=True)
+            ).values_list("product__id", flat=True)
 
-        elif standard == 'review':
+        elif standard == "review":
             product_ids = Review.objects.filter(
                 pub_date__range=[start_date, end_date]
-            ).values_list('product__id',flat=True)
+            ).values_list("product__id", flat=True)
 
         counter = Counter(product_ids)
         result_product_ids = []
         for count in counter.most_common(5):
             result_product_ids.append(count[0])
 
-        queryset = self.get_queryset().filter(
-            id__in = result_product_ids
-        )
+        queryset = self.get_queryset().filter(id__in=result_product_ids)
 
         serializer = self.get_serializer(queryset, many=True)
 
