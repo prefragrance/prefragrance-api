d7a6a8c7a1b3af2fc77b0fa5beda1df829391321
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2022-10-04 14:33:34 +0900

    chore: 불필요 주석 삭제

--- search/serializers/recommend_search_serializer.py
+++ search/serializers/recommend_search_serializer.py
@@ -6,12 +6,6 @@ from search.models import RecommendSearch
 class RecommendSearchSerializer(ModelSerializer):
     """Serializer definition for RecommendSearch Model."""
     
-    # '''Serializer로 전달된 quaryset으로 부터 value만 추출해 리스트로 만들어 리스트를 리턴'''
-    # def to_representation(self, instance):
-    #     original_data = super().to_representation(instance)
-    #     modified_data = [i.values() for i in len(original_data)+1]
-    #     return modified_data
-    
     class Meta:
         """Meta definition for RecommendSearchSerializer."""
 
