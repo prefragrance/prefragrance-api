41a385ef0d1457743c4e5b33a38c36db99d81ab8
Jin409 <sualng123@gmail.com>
2022-11-04 15:15:45 +0900

    chore: exception handler 관련 초기 세팅

---
+++ webapp/config/exception/exception_handler.py
@@ -0,0 +1,56 @@
+import logging
+from http import HTTPStatus
+from typing import Any
+
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
+from rest_framework import exceptions
+from rest_framework.views import Response, exception_handler, set_rollback
+
+logger = logging.getLogger("django.db.backends")
+
+def exception_handler(exc, context):
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound()
+    elif isinstance(exc, PermissionDenied):
+        exc = exceptions.PermissionDenied()
+
+    if isinstance(exc, exceptions.APIException):
+        headers = {}
+        if getattr(exc, "auth_header", None):
+            headers["WWW-Authenticate"] = exc.auth_header
+        if getattr(exc, "wait", None):
+            headers["Retry-After"] = "%d" % exc.wait
+
+        if isinstance(exc.detail, (list, dict)):
+            data = exc.detail
+        else:
+            data = {"detail": exc.detail}
+
+        set_rollback()
+        return Response(data, status=exc.status_code, headers=headers)
+
+    return None
+
+
+def api_exception_handler(exc: Exception, context: dict[str, Any]) -> Response:
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        http_code_to_message = {v.value: v.description for v in HTTPStatus}
+        error_payload = {
+            "error": {
+                "status_code": 0,
+                "message": "",
+                "details": [],
+            }
+        }
+        error = error_payload["error"]
+        status_code = response.status_code
+
+        error["status_code"] = status_code
+        error["message"] = http_code_to_message[status_code]
+        error["details"] = response.data
+        response.data = error_payload
+
+    return response
