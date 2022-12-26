76faab31f0a106feb566db0a5ecb802f87834d67
yoojung.choi <green980611@naver.com>
2022-12-27 00:19:43 +0900

    feat: 아이디, 비밀번호 관련 validator 추가

--- webapp/accounts/adapters.py
+++ webapp/accounts/adapters.py
@@ -6,11 +6,9 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         data = form.cleaned_data
         user = super().save_user(request, user, form, False)
         user.nickname = data.get("nickname")
-        user.name = data.get("name")
         user.age = data.get("age")
         user.gender = data.get("gender")
         user.agree_prefragrance = data.get("agree_prefragrance")
         user.agree_personal_required = data.get("agree_personal_required")
-        user.agree_personal_optional = data.get("agree_personal_optional")
         user.save()
         return user
--- webapp/accounts/serializers/user_register_serializer.py
+++ webapp/accounts/serializers/user_register_serializer.py
@@ -1,11 +1,13 @@
+from accounts.validators import validate_password, validate_username
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
 class CustomRegisterSerializer(RegisterSerializer):
+    username = serializers.CharField(validators=[validate_username])
+    password1 = serializers.CharField(validators=[validate_password])
     nickname = serializers.CharField(required=False, max_length=50)
-    name = serializers.CharField(required=False, max_length=200)
     age = serializers.IntegerField(required=False)
     gender = serializers.ChoiceField(
         required=False,
@@ -17,27 +19,25 @@ class CustomRegisterSerializer(RegisterSerializer):
     )
     agree_prefragrance = serializers.BooleanField(label="취향 이용약관 동의 (필수)")
     agree_personal_required = serializers.BooleanField(label="개인정보 수집 및 이용 동의 (필수)")
-    agree_personal_optional = serializers.BooleanField(label="개인정보 수집 및 이용 동의 (선택)")
 
     def validate(self, data):
         if data["agree_prefragrance"] != True:
             raise serializers.ValidationError(_("이용약관 동의는 필수입니다."))
         elif data["agree_personal_required"] != True:
             raise serializers.ValidationError(_("개인정보 수집 및 이용 동의는 필수입니다."))
+
+        if data["password1"] != data["password2"]:
+            raise serializers.ValidationError(_("비밀번호가 일치하지 않습니다."))
         return data
 
     def get_cleaned_data(self):
         data = super().get_cleaned_data()  # username, password, email이 디폴트
         data["nickname"] = self.validated_data.get("nickname", "")
-        data["name"] = self.validated_data.get("name", "")
         data["age"] = self.validated_data.get("age", "")
         data["gender"] = self.validated_data.get("gender", "")
         data["agree_prefragrance"] = self.validated_data.get("agree_prefragrance", "")
         data["agree_personal_required"] = self.validated_data.get(
             "agree_personal_required", ""
-        )
-        data["agree_personal_optional"] = self.validated_data.get(
-            "agree_personal_optional", ""
         )
 
         return data
