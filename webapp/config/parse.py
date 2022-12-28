b1d44a9d2b90ad9c1e3b4194c5c26291c8eadd9c
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2022-12-28 16:42:34 +0900

    test: 작업 준비위한 이전 작업물 커밋

--- webapp/config/parse.py
+++ webapp/config/parse.py
@@ -16,18 +16,24 @@ perfumes.rename(columns = {'main accords' : 'main_accords'}, inplace = True)
 print(perfumes.head())
 
 for perfume in perfumes.itertuples():
-    perfume_product = Product.objects.create(
-        name = perfume.name,
-        category = Category.objects.create(name = perfume.category), #Foreign Key Referenced Feature는 어떻게 다뤄야하는가? 이 라인 다시 살펴볼 것.)
-        producer = perfume.company,
-        thumbnail_url = perfume.image,
-        rate = perfume.rating,
-        )
-    for cd in perfume.main_accords:
-        perfume_code = ProductCode.objects.create(
-            product = perfume.name,
-            code = ProductCode.objects.create(cd)
-        )
+    # perfume_product = Product.objects.create(
+    #     name = perfume.name,
+    #     category = Category.objects.create(name = perfume.category), #Foreign Key Referenced Feature는 어떻게 다뤄야하는가? 이 라인 다시 살펴볼 것.)
+    #     producer = perfume.company,
+    #     thumbnail_url = perfume.image,
+    #     rate = perfume.rating,
+    #     )
+    print(perfume.main_accords)
+    print(type(perfume.main_accords))
+        # print(cd)
+        # print(type(cd))
+'''
+
+    code = ProductCode.objects.create(cd)
+
+    return getattr(self.get_queryset(), name)(*args, **kwargs)
+TypeError: QuerySet.create() takes 1 positional argument but 2 were given
+'''
 
 # notes = pd.read_csv('/Users/hoyeon/dev/취향/prefragrance-api/webapp/config/all_notes.csv')
 # notes.rename(columns = {'Note Name' : 'name'}, inplace = True)
@@ -38,32 +44,3 @@ for perfume in perfumes.itertuples():
 #     note = Code.objects.create(
 #         name = note.name
 #     )
-'''    
-class Product(models.Model):
-    id = models.AutoField(primary_key=True, null=False, blank=False)  # primary key
-    category = models.ForeignKey(
-        "product.Category", null=True, blank=True, on_delete=models.CASCADE
-    )
-    thumbnail_url = models.URLField(null=True, blank=True)
-    name = models.CharField(max_length=400, null=False, blank=False)
-    producer = models.CharField(max_length=400, null=False, blank=False)
-    tags = TaggableManager(blank=True)
-    codes = models.ManyToManyField(
-        "product.Code",
-        related_name="codes",
-        through="product.ProductCode",
-    )
-    
-    ###NULL OK
-    feedback_cnt = models.IntegerField(default=0)
-    review_cnt = models.IntegerField(default=0)
-    visit_cnt = models.IntegerField(default=0)
-    
-    rate_sum = models.FloatField(null=True, blank=True)
-    rate = models.FloatField(default=0)
-    liked_users = models.ManyToManyField(
-        "accounts.User",
-        through="product.ProductFeedback",
-        related_name="liked_products",
-    )
-'''
--- webapp/product/models/product_code.py
+++ webapp/product/models/product_code.py
@@ -2,6 +2,7 @@ from django.db import models
 from product.models import Product, Code
 
 class ProductCode(models.Model):
+        
     id = models.AutoField(primary_key=True, null=False, blank=False)
     product = models.ForeignKey(Product, null = False, blank = False, on_delete = models.CASCADE)
     code = models.ForeignKey(Code, null = False, blank = False, on_delete = models.CASCADE)
@@ -14,3 +15,52 @@ class ProductCode(models.Model):
                 name="unique_product_code",
             )
         ]
+        
+'''
+choicefield 참고용 리뷰 모델
+class Review(models.Model):
+    class Season(models.IntegerChoices):
+        SPRING = 1
+        SUMMER = 2
+        AUTUMN = 3
+        WINTER = 4
+
+    class Time(models.IntegerChoices):
+        DAY = 1
+        NIGHT = 2
+
+    class Duration(models.IntegerChoices):
+        LOW = 1
+        MID = 2
+        HIGH = 3
+
+    class Strength(models.IntegerChoices):
+        LOW = 1
+        MID = 2
+        HIGH = 3
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="사용자")
+    product = models.ForeignKey(
+        "product.Product",
+        on_delete=models.CASCADE,
+        verbose_name="제품",
+        related_name="reviews",
+    )
+    season = models.IntegerField(choices=Season.choices)
+    time = models.IntegerField(choices=Time.choices)
+    duration = models.IntegerField(choices=Duration.choices)
+    strength = models.IntegerField(choices=Strength.choices)
+    content = models.CharField(max_length=1500, verbose_name="내용")
+    rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    pub_date = models.DateTimeField(auto_now=True, verbose_name="날짜")
+    feedback_cnt = models.PositiveIntegerField(default=0)
+    liked_users = models.ManyToManyField(
+        "accounts.User", through="review.ReviewFeedback", related_name="liked_reviews"
+    )
+    tags = TaggableManager(blank=True)
+
+
+
+
+
+'''
