967f7671cf359b99ffb4a7dddc651ac7a4a9a65b
Jin409 <sualng123@gmail.com>
2022-12-30 00:32:14 +0900

    feat: 리뷰 복사하는 어드민 액션

--- webapp/review/admin/review.py
+++ webapp/review/admin/review.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 
+from accounts.models import User
+from product.models import Product
 from review.models import Review, ReviewFeedback
 
 
 class LikeInline(admin.StackedInline):
     model = ReviewFeedback
 
+def copy_review(self, request, queryset):
+    review = queryset[0]
+
+    for product in Product.objects.all():
+        review.pk = None
+        review.product = product
+        review.save()
+
+
+copy_review.short_description = "리뷰를 복사합니다."
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
@@ -22,6 +34,7 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display_links = ("user",)
     search_fields = ("product",)
     ordering = ("pub_date",)
+    actions = (copy_review,)
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related("tags")
