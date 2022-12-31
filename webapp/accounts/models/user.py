51411777bf93166a3aef657f70d25ae108c2326a
yoojung.choi <green980611@naver.com>
2022-12-31 10:19:33 +0300

    chore: 불필요한 코드 수정

--- webapp/accounts/models/user.py
+++ webapp/accounts/models/user.py
@@ -1,6 +1,6 @@
 from accounts.validators import validate_username
-from django.contrib.auth.models import (AbstractBaseUser, AbstractUser,
-                                        BaseUserManager, PermissionsMixin)
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
